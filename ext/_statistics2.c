91f3b3af97d34751113b88d873528bee6c06661a
Ryosuke Watanabe <suzukai74@gmail.com>
2024-04-03 12:43:36 +0900

    test

--- ext/_statistics2.c
+++ ext/_statistics2.c
@@ -701,7 +701,7 @@ static VALUE rb_pfX_(mod, n1, n2, x)
 
 /* discrete distributions */
 
-int static VALUE rb_bindens(mod, n, p, x)
+static VALUE rb_bindens(mod, n, p, x)
     VALUE mod, n, p, x;
 {
     Need_Float(p);
